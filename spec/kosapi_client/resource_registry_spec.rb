6923c57d66d64fe64752022240f22e0ba8fafb1b
Tibor Szolar <tibor.szolar@seznam.cz>
2013-11-07 16:31:39 +0100

    Resource registry spec improved.

--- spec/kosapi_client/resource_registry_spec.rb
+++ spec/kosapi_client/resource_registry_spec.rb
@@ -3,18 +3,16 @@ require 'spec_helper'
 describe KOSapiClient::ResourceRegistry do
   let(:client) { double() }
   subject(:registry) { KOSapiClient::ResourceRegistry.new(client) }
+  let(:test_class_instance) { double() }
+  let(:test_class) { double(name: 'TestClass', new: test_class_instance) }
 
-  it 'registers all resources from module Resources' do
-
+  it 'registers resources from module Resources' do
+    expect(client.respond_to?(:course_event)).to be_true
   end
 
   it 'registers resource instance' do
-    test_class_instance = double()
-    test_class = double(name: 'TestClass', new: test_class_instance)
     registry.register_resource(test_class)
-
     expect(registry.get_resource(:test_class)).to be test_class_instance
   end
 
-  it 'returns registered resource instance'
 end
