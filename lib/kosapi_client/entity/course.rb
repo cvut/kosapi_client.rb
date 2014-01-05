bbfb6ee63d2cecaf85421ab842d52f853bbfccdc
Tibor Szolar <tibor.szolar@seznam.cz>
2014-01-05 20:47:38 +0100

    Course field fixes.

--- lib/kosapi_client/entity/course.rb
+++ lib/kosapi_client/entity/course.rb
@@ -1,10 +1,10 @@
 module KOSapiClient
   module Entity
     class Course < BaseEntity
-      map_data :allowedEnrollmentCount, Integer
-      map_data :approvalDate, Time
-      map_data :classesLang, Enum
-      map_data :classesType, Enum #todo: possible array?
+      map_data :allowed_enrollment_count, Integer
+      map_data :approval_date, Time
+      map_data :classes_lang, Enum
+      map_data :classes_type, Enum #todo: possible array?
       map_data :code
       map_data :completion, Enum
       map_data :credits, Integer
@@ -12,20 +12,20 @@ module KOSapiClient
       map_data :description
       map_data :homepage
       map_data :keywords
-      map_data :lecturesContents
+      map_data :lectures_contents
       map_data :literature
       map_data :name
       map_data :note
       map_data :objectives
-      map_data :programmeType, Enum
+      map_data :programme_type, Enum
       map_data :range
       map_data :requirements
       map_data :season, Enum
       map_data :state, Enum
-      map_data :studyForm, Enum
-      map_data :superiorCourse, Link
+      map_data :study_form, Enum
+      map_data :superior_course, Link
       map_data :subcourses, Link
-      map_data :tutorialsContents
+      map_data :tutorials_contents
       map_data :instance #todo
 
     end
