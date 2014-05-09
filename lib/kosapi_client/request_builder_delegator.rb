364db384a93ecbc6a0c973f58c0576e09f295de5
Tibor Szolár <tibor.szolar@seznam.cz>
2014-05-09 23:55:29 +0200

    Working delegation in RequestBuilderDelegator.

--- lib/kosapi_client/request_builder_delegator.rb
+++ lib/kosapi_client/request_builder_delegator.rb
@@ -6,26 +6,37 @@ module KOSapiClient
       @response = nil
     end
 
+    alias :super_method_missing :method_missing
+
     def method_missing(method, *args)
       if @response.nil?
-        if @request_builder.respond_to?(method)
-          @request_builder.send(method, *args)
-        else
-          @request_builder.finalize
-          @response = @request_builder.response
-          @response.send(method, *args)
-        end
+        delegate_to_builder(method, *args)
       else
-        if @response.respond_to?(:method)
-          @response.send(method, *args)
-        else
-          super
-        end
+        delegate_to_response(method, *args)
       end
     end
 
     def respond_to?(method)
 
+    end
+
+    private
+    def delegate_to_response(method, *args)
+      if @response.respond_to?(method)
+        @response.send(method, *args)
+      else
+        super_method_missing(method, *args)
+      end
+    end
+
+    def delegate_to_builder(method, *args)
+      if @request_builder.respond_to?(method)
+        @request_builder.send(method, *args)
+      else
+        @request_builder.finalize
+        @response = @request_builder.response
+        delegate_to_response(method, *args)
+      end
     end
 
   end
--- spec/kosapi_client/request_builder_delegator_spec.rb
+++ spec/kosapi_client/request_builder_delegator_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe KOSapiClient::RequestBuilderDelegator do
 
   let(:response) { double(:response) }
-  let(:builder) { double(:builder, response: response) }
+  let(:builder) { double(:builder, response: response, finalize: nil) }
   subject(:delegator) { KOSapiClient::RequestBuilderDelegator.new(builder) }
 
   it 'delegates methods which builder responds to, to the builder' do
@@ -11,13 +11,22 @@ describe KOSapiClient::RequestBuilderDelegator do
     delegator.foo
   end
 
-  it 'evaluates RequestBuilder, when RequestBuilder doesn"t respond to a delegated method' do
+  it 'evaluates builder when builder is not responding to a delegated method' do
     expect(builder).to receive(:finalize)
     expect(response).to receive(:bar)
     delegator.bar
   end
 
-  it 'when RequestBuilder is evaluated, it delegates all methods to response'
+  it 'delegates all methods to response once RequestBuilder is evaluated' do
+    allow(response).to receive(:bar)
+    expect(builder).not_to receive(:foo)
+    delegator.bar
+    expect { delegator.foo }.to raise_error(NoMethodError)
+  end
+
+  it 'throws error when method is not defined on builder or response' do
+    expect { delegator.foo }.to raise_error(NoMethodError)
+  end
 
 
 end
