ddbf0269b5bea0e0dcc1c406460f9d382fa5bf27
Tibor Szolár <tibor.szolar@seznam.cz>
2014-05-12 00:41:48 +0200

    Separated BuilderDelegator specs into separate contexts based on responsible methods.

--- spec/kosapi_client/request_builder_delegator_spec.rb
+++ spec/kosapi_client/request_builder_delegator_spec.rb
@@ -6,37 +6,46 @@ describe KOSapiClient::RequestBuilderDelegator do
   let(:builder) { double(:builder, response: response, finalize: nil) }
   subject(:delegator) { KOSapiClient::RequestBuilderDelegator.new(builder) }
 
-  it 'delegates methods which builder responds to, to the builder' do
-    expect(builder).to receive(:foo)
-    delegator.foo
+  describe '#method_missing' do
+
+    it 'delegates methods which builder responds to, to the builder' do
+      expect(builder).to receive(:foo)
+      delegator.foo
+    end
+
+    it 'evaluates builder when builder is not responding to a delegated method' do
+      expect(builder).to receive(:finalize)
+      expect(response).to receive(:bar)
+      delegator.bar
+    end
+
+    it 'delegates all methods to response once RequestBuilder is evaluated' do
+      allow(response).to receive(:bar)
+      expect(builder).not_to receive(:foo)
+      delegator.bar
+      expect { delegator.foo }.to raise_error(NoMethodError)
+    end
+
+    it 'throws error when method is not defined on builder or response' do
+      expect { delegator.foo }.to raise_error(NoMethodError)
+    end
+
   end
 
-  it 'evaluates builder when builder is not responding to a delegated method' do
-    expect(builder).to receive(:finalize)
-    expect(response).to receive(:bar)
-    delegator.bar
-  end
 
-  it 'delegates all methods to response once RequestBuilder is evaluated' do
-    allow(response).to receive(:bar)
-    expect(builder).not_to receive(:foo)
-    delegator.bar
-    expect { delegator.foo }.to raise_error(NoMethodError)
-  end
+  describe '#respond_to_missing?' do
 
-  it 'throws error when method is not defined on builder or response' do
-    expect { delegator.foo }.to raise_error(NoMethodError)
-  end
+    it 'responds to methods on builder' do
+      allow(builder).to receive(:foo)
+      expect(delegator).to respond_to(:foo)
+    end
 
-  it 'responds to methods on builder' do
-    allow(builder).to receive(:foo)
-    expect(delegator).to respond_to(:foo)
-  end
+    it 'responds to methods on response when evaluated' do
+      allow(response).to receive(:foo)
+      delegator.foo
+      expect(delegator).to respond_to(:foo)
+    end
 
-  it 'responds to methods on response when evaluated' do
-    allow(response).to receive(:foo)
-    delegator.foo
-    expect(delegator).to respond_to(:foo)
   end
 
 end
