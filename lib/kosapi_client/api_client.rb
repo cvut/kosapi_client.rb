881aed0d45e2c920697e372c619de64508cb844b
Tibor Szolár <tibor.szolar@seznam.cz>
2014-05-11 03:51:56 +0200

    ApiClient now creates RequestBuilders.

--- lib/kosapi_client/api_client.rb
+++ lib/kosapi_client/api_client.rb
@@ -5,11 +5,14 @@ module KOSapiClient
   def self.new(credentials, root_url = DEFAULT_KOSAPI_ROOT_URL)
     http_adapter = OAuth2HttpAdapter.new(credentials, root_url)
     http_client = HTTPClient.new(http_adapter)
-    KOSapiClient::ApiClient.new(http_client)
+    KOSapiClient::ApiClient.new(http_client, root_url)
   end
 
 
   class ApiClient
+    include ResourceMapper
+
+    #resource :parallels
 
     # Include all submodules of KOSapiClient::Resource to this class
     KOSapiClient::Resource.constants.map{|m| KOSapiClient::Resource.const_get(m) }.select { |m| m.is_a? Module }.each{ |m| include m }
@@ -21,9 +24,15 @@ module KOSapiClient
     ##
     # Creates a new KOSapi client.
     #
-    def initialize(http_client)
+    def initialize(http_client, root_url = DEFAULT_KOSAPI_ROOT_URL)
       @http_client = http_client
+      @root_url = root_url
     end
+
+    def create_builder(resource_name)
+      RequestBuilder.new(@root_url + resource_name.to_s, @http_client)
+    end
+
 
   end
 end
--- spec/kosapi_client/api_client_spec.rb
+++ spec/kosapi_client/api_client_spec.rb
@@ -6,4 +6,9 @@ describe KOSapiClient::ApiClient do
   it 'responds to api resource methods' do
     expect(client).to respond_to :course_events
   end
+
+  it 'creates default builder when no custom builder is defined' do
+    builder = client.create_builder(:foo)
+    expect(builder).to be_an_instance_of KOSapiClient::RequestBuilder
+  end
 end
