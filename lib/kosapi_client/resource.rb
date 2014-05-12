9039170bf66dfb590ab2da5e706c8a36e0437870
Tibor Szolar <tibor.szolar@seznam.cz>
2014-05-12 16:02:47 +0200

    Removed obsolete modules.

--- lib/kosapi_client/resource.rb
+++ lib/kosapi_client/resource.rb
@@ -1,6 +1,3 @@
-require 'kosapi_client/resource/course_events'
-require 'kosapi_client/resource/courses'
-require 'kosapi_client/resource/parallels'
 require 'kosapi_client/request_builder'
 require 'kosapi_client/resource/courses_builder'
 require 'kosapi_client/resource/parallels_builder'
