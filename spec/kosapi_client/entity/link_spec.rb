0d81c950951ce55cf42fb329acc371ce1f6d544f
Tibor Szolar <tibor.szolar@seznam.cz>
2014-05-12 19:10:15 +0200

    Link spec improvement.

--- spec/kosapi_client/entity/link_spec.rb
+++ spec/kosapi_client/entity/link_spec.rb
@@ -1,9 +1,19 @@
-require 'rspec'
+require 'spec_helper'
 
 describe KOSapiClient::Entity::Link do
 
-  it 'should act as lazy proxy' do
+  Link = KOSapiClient::Entity::Link
 
-    true.should == false
+  subject(:link) { Link.parse({href: 'http://example.com', __content__: 'Example Site', rel: 'next'}) }
+
+  describe '.parse' do
+
+    it 'can be parsed from a hash' do
+      expect(link.href).to eq 'http://example.com'
+      expect(link.title).to eq 'Example Site'
+      expect(link.rel).to eq 'next'
+    end
+
   end
+
 end
