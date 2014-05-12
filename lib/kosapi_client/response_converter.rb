5335361ec091456bd1bed03f4b8a4a2287f6d79b
Tibor Szolar <tibor.szolar@seznam.cz>
2014-05-12 19:12:04 +0200

    A bit of ResponseConverter docs.

--- lib/kosapi_client/response_converter.rb
+++ lib/kosapi_client/response_converter.rb
@@ -1,9 +1,20 @@
 module KOSapiClient
+
+  # This class converts parsed response in hash format
+  # (wrapped in Response) into domain Ruby objects.
+  # Root domain object type is
+  # determined at runtime based on API response.
+
   class ResponseConverter
 
     def initialize()
 
     end
+
+    # Returns processed response converted into domain objects.
+    # When called resource can return collection of objects
+    # @param response [HTTPResponse] API response in hash format
+    # @return [ResultPage] ResultPage or single domain object
 
     def process_response(response)
       #response = preprocess(response)
@@ -15,6 +26,7 @@ module KOSapiClient
       end
     end
 
+    private
     def convert_type(response_part, type)
       type.parse(response_part)
     end
