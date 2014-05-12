ee64cc3e06b0789ea994de83a53511774ebe306a
Tibor Szolár <tibor.szolar@seznam.cz>
2014-05-12 02:18:38 +0200

    Requires...

--- lib/kosapi_client/resource.rb
+++ lib/kosapi_client/resource.rb
@@ -1,6 +1,9 @@
 require 'kosapi_client/resource/course_events'
 require 'kosapi_client/resource/courses'
 require 'kosapi_client/resource/parallels'
+require 'kosapi_client/request_builder'
+require 'kosapi_client/resource/courses_builder'
+require 'kosapi_client/resource/parallels_builder'
 
 module KOSapiClient
   module Resource
