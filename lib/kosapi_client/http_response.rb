f327fcdd747036132e918aba89de84e3a11237e8
Tibor Szolar <tibor.szolar@seznam.cz>
2014-05-13 19:01:40 +0200

    Removed duplicated methods.

--- lib/kosapi_client/http_response.rb
+++ lib/kosapi_client/http_response.rb
@@ -3,8 +3,6 @@ module KOSapiClient
 
     include Enumerable
 
-    attr_reader :contents
-    attr_accessor :offset
 
     def initialize(result, preprocessor = ResponsePreprocessor.new, converter = ResponseConverter.new)
       @result = result
@@ -37,27 +35,5 @@ module KOSapiClient
       items.first
     end
 
-    def each(&block)
-      items.each(&block)
-    end
-
-    def detect_type
-      type_str = sample_entry[:xsi_type]
-      extract_type(type_str)
-    end
-
-    def offset
-
-
-    end
-
-    def sample_entry
-      items.first
-    end
-
-    def extract_type(type_str)
-      type_name = type_str.camelize
-      Entity.const_get(type_name)
-    end
   end
 end
