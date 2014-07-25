5aa29dde092889280dcd964fc8a26e06ff6e5245
Tibor Szolár <tibor.szolar@seznam.cz>
2014-07-25 04:04:04 +0200

    ResponseConverter specs refactoring.

--- spec/kosapi_client/response_converter_spec.rb
+++ spec/kosapi_client/response_converter_spec.rb
@@ -4,28 +4,33 @@ describe KOSapiClient::ResponseConverter do
 
   subject(:converter) { described_class.new }
 
+  describe '#convert' do
 
-  describe '#convert_paginated' do
+    context 'with paginated response' do
 
-    let(:items) { [{xsi_type: 'courseEvent', capacity: 70}, {xsi_type: 'courseEvent', capacity: 40}] }
+      let(:api_response) { double(is_paginated?: true, items: [{xsi_type: 'courseEvent', capacity: 70}, {xsi_type: 'courseEvent', capacity: 40}]) }
 
-    it 'processes paginated response' do
-      result = converter.convert_paginated(items)
-      expect(result).to be_an_instance_of(KOSapiClient::Entity::ResultPage)
+      it 'processes paginated response' do
+        result = converter.convert(api_response)
+        expect(result).to be_an_instance_of(KOSapiClient::Entity::ResultPage)
+      end
+
+
     end
 
+    context 'with single entry response' do
+
+      let(:api_response) { double(is_paginated?: false, item: {xsi_type: 'courseEvent', capacity: 70}) }
+
+      it 'processes non-paginated response' do
+        result = converter.convert(api_response)
+        expect(result).to be_an_instance_of(KOSapiClient::Entity::CourseEvent)
+      end
+
+    end
 
   end
 
-  describe '#convert_single' do
 
-    let(:item) { {xsi_type: 'courseEvent', capacity: 70} }
-
-    it 'processes non-paginated response' do
-      result = converter.convert_single(item)
-      expect(result).to be_an_instance_of(KOSapiClient::Entity::CourseEvent)
-    end
-
-  end
 
 end
