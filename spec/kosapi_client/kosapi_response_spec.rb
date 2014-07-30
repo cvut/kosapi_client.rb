6233da3c8bdf3a37017f2ef96ee1a03eab38b020
Tibor Szolar <tibor.szolar@seznam.cz>
2014-07-30 20:35:40 +0200

    Implemented next Link parsing in KOSapiResponse.

--- spec/kosapi_client/kosapi_response_spec.rb
+++ spec/kosapi_client/kosapi_response_spec.rb
@@ -3,9 +3,10 @@ require 'spec_helper'
 describe KOSapiClient::KOSapiResponse do
 
   subject(:response) { KOSapiClient::KOSapiResponse.new(result) }
+  let(:client) { instance_double(KOSapiClient::HTTPClient) }
 
   context 'with paginated response' do
-    let(:result) { {atom_feed: {atom_entry: [:entry1, :entry2]}} }
+    let(:result) { {atom_feed: {atom_entry: [:entry1, :entry2], atom_link: [{rel: 'prev', href: 'courses/?offset=0&limit=10'}, {rel: 'next', href: 'courses/?offset=20&limit=10'}]}} }
 
     describe '#is_paginated?' do
 
@@ -27,6 +28,48 @@ describe KOSapiClient::KOSapiResponse do
 
       it 'returns first entry' do
         expect(response.item).to eq :entry1
+      end
+
+    end
+
+    describe '#next_link' do
+
+      it 'parses next link from feed' do
+        link = response.next_link(client)
+        expect(link.href).to eq 'courses/?offset=20&limit=10'
+        expect(link.rel).to eq 'next'
+      end
+
+      it 'injects http client' do
+        link = response.next_link(client)
+        expect(client).to receive(:send_request)
+        link.follow
+      end
+
+      context 'with single link' do
+        let(:result) { {atom_feed: {atom_link: {rel: 'next', href: 'courses/?offset=10&limit=10'}}} }
+
+        it 'parses next link from feed' do
+          link = response.next_link(client)
+          expect(link.href).to eq 'courses/?offset=10&limit=10'
+          expect(link.rel).to eq 'next'
+        end
+      end
+
+      context 'with no links' do
+        let(:result) { {atom_feed: {}} }
+
+        it 'returns null' do
+          expect(response.next_link(client)).not_to be
+        end
+      end
+
+      context 'with only prev link' do
+        let(:result) { {atom_feed: {atom_link: {rel: 'prev', href: 'courses/?offset=10&limit=10'}}} }
+
+        it 'returns null' do
+          expect(response.next_link(client)).not_to be
+        end
       end
 
     end
