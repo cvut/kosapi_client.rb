c30b363b309d421fee5d44d4e5dfb2c6d43e8a99
Tomáš Polívka <tomas.polivka@designeo.cz>
2015-04-12 00:23:19 +0200

    Fixed bad merge

--- lib/kosapi_client/kosapi_client.rb
+++ lib/kosapi_client/kosapi_client.rb
@@ -1,44 +1,45 @@
 module KOSapiClient
 
-  DEFAULT_KOSAPI_BASE_URL = 'https://kosapi.fit.cvut.cz/api/3'
-
   singleton_class.class_eval do
 
-    attr_reader :client
-    
-    alias_method :to_str, :to_s
-
-    def new(credentials, base_url = DEFAULT_KOSAPI_BASE_URL)
-      http_adapter = OAuth2HttpAdapter.new(credentials, base_url)
-      http_client = HTTPClient.new(http_adapter)
-      ApiClient.new(http_client, base_url)
+    def new(options = {})
+      ApiClient.new(Configuration.new(options))
     end
 
     def configure
-      config = Configuration.new
+      reset
       yield config
-      @client = new(config.credentials)
+      self
+    end
+
+    def client
+      @client ||= ApiClient.new(config)
     end
 
     # Calling this method clears stored ApiClient instance
     # if configured previously.
     def reset
+      @config = nil
       @client = nil
     end
 
     def method_missing(method, *args, &block)
-      if @client.nil?
-        raise "Client not configured. Either you forgot to call configure or you have typo in method name '#{method}'."
-      end
-      if @client.respond_to?(method)
-        @client.send(method, *args, &block)
+      if client.respond_to?(method)
+        client.send(method, *args, &block)
       else
         super
       end
     end
 
     def respond_to_missing?(method_name, include_private = false)
-      @client.respond_to?(method_name, include_private)
+      client.respond_to?(method_name, include_private)
     end
+
+    private
+    def config
+      @config ||= Configuration.new
+    end
+
   end
+
 end
