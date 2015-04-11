7e4c7d95e03e94c3f61ee8cd7419a3e6f96b2062
Tomáš Polívka <tomas.polivka@designeo.cz>
2015-04-12 00:15:46 +0200

    Fixed comments

--- lib/kosapi_client/resource/courses_builder.rb
+++ lib/kosapi_client/resource/courses_builder.rb
@@ -9,16 +9,8 @@ module KOSapiClient
 
       ###
       # GET /courses/{code}/parallels
-      # Vrátí paralelky (instance) předmětu
-      #   a) v aktuálním semestru,
-      #   b) ve zvolených semestrech parametrem +sem+,
-      #   c) nebo pro všechny semestry (parametr sem=none).
       #
       # URI: https://kosapi.fit.cvut.cz/api/3/courses/{code}/parallels/
-      #
-      # Typ obsahu: Parallel
-      # Parametry: sem, fields, lang, limit, locEnums, multilang, offset, orderBy, query
-      #
       # @link: https://kosapi.fit.cvut.cz/projects/kosapi/wiki/Courses#GET-coursescodeparallels
       ##
       # Example:
@@ -33,16 +25,8 @@ module KOSapiClient
 
       ###
       # GET /courses/{code}/students
-      # Vrátí studenty zapsané na (instanci) předmětu
-      #   a) v aktuálním semestru,
-      #   b) ve zvolených semestrech parametrem +sem+,
-      #   c) nebo pro všechny semestry (parametr sem=none).
       #
       # URI: https://kosapi.fit.cvut.cz/api/3/courses/{code}/students/
-      #
-      # Typ obsahu: Student
-      # Parametry: sem, fields, lang, limit, locEnums, multilang, offset, orderBy, query
-      #
       # @link: https://kosapi.fit.cvut.cz/projects/kosapi/wiki/Courses#GET-coursescodestudents
       ##
       # Example:
@@ -57,15 +41,8 @@ module KOSapiClient
 
       ###
       # GET /courses/{code}/instances
-      # Vrátí všechny instance daného předmětu – pouze atributy instance, nikoli obecné předmětu.
-      # Ve většině případech budete chtít použít spíše zdroj /courses/{code} s parametrem +sem+.
       #
       # URI: https://kosapi.fit.cvut.cz/api/3/courses/{code}/instances/
-      #
-      # Typ obsahu: Coursin
-      # Proměnné: {code} kód předmětu
-      # Parametry: fields, lang, multilang, locEnums
-      #
       # @link: https://kosapi.fit.cvut.cz/projects/kosapi/wiki/Courses#GET-coursescodeinstances
       ##
       # Example:
